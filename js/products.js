26406494a8de07da8dab3303f1335324dd217c85
Artem Pyatnica <>
2021-05-29 12:33:33 +0000

    add some stuff

--- js/products.js
+++ js/products.js
@@ -1,32 +1,10 @@
-const productsJson = 
-`[
-    {
-        "id": "1",
-        "title": "Mini Gir",
-        "image": "img/gir.png",
-        "description": "Mini Gir Sticker",
-        "price": 49.99
-    },
-    {
-        "id": "2",
-        "title": "Gir Happy",
-        "image": "img/gir-happy.png",
-        "description": "Happy Gir sticker",
-        "price": 19.99
-    },
-    {
-        "id": "3",
-        "title": "Zir",
-        "image": "img/zim.png",
-        "description": "Zim sticker",
-        "price": 24.99
-    }
-]`;
 
-function renderProducts(products) {
+let products;
+
+function renderProducts() {
     const productsContainer = document.querySelector('.product-list');
     let html = '';
-    for (const product of products ) {
+    for (const product of  ) {
         html += `<article>
         <img src="${product.image}" alt="${product.title}">
         <h3>${product.title}</h3>
@@ -38,4 +16,12 @@ function renderProducts(products) {
     productsContainer.innerHTML = html;
 }
 
-renderProducts(JSON.parse(productsJson));
+renderProducts();
+
+
+
+function fetchProducts() {
+  fetch('products.json')
+    .then(response => response.json())
+    .then(productsFromServer => products = productsFromServer);
+}
